f8c47770695fc651afc775c47f3e9070919a729a
martoko <martoko@martoko.dk>
2021-01-17 14:26:17 +0100

    Change kernel size from 5x5 to 3x3

--- code/recognizer/model.py
+++ code/recognizer/model.py
@@ -9,15 +9,15 @@ class KanjiRecognizer(nn.Module):
     def __init__(self, input_dimensions: int, output_dimensions: int):
         super(KanjiRecognizer, self).__init__()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=5)  # 32 -> 28
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5)  # 14 -> 10
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5)  # 10 -> 6
-        self.linear = nn.Linear(64 * 6 * 6, output_dimensions)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3)  # 32 -> 30
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)  # 15 -> 13
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)  # 13 -> 11
+        self.linear = nn.Linear(64 * 11 * 11, output_dimensions)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
-        x = x.view(-1, 64 * 6 * 6)
+        x = x.view(-1, 64 * 11 * 11)
         x = self.linear(x)
         return x
